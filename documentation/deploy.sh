c6fe48280341f9c916b3541695e23333f04dc290
Alexander Birkner <alexander.birkner@marbis.net>
2016-11-30 18:02:50 +0100

    Fixed Deployment Script for Documentation Update

--- documentation/deploy.sh
+++ documentation/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR/.."
+
 echo "Publish new Documentation"
 git push origin `git subtree split --prefix documentation/site/ master`:gh-pages --force
-
